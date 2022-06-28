e969305ca57f94812007964a1628e0b5f2793ac1
Jeremy Zucker <djinnome@gmail.com>
2022-06-28 14:17:25 -0700

    ran lint to correct formatting (and found a few syntax errors along the way

--- tests/test_mentos.py
+++ tests/test_mentos.py
@@ -1,12 +1,17 @@
 import unittest
-import libsbml
-from  mentos.maximum_entropy import maximum_entropy_pyomo_relaxed, get_params
-class TestMaximumEntropy(unittest.TestCase):
 
+import libsbml
+
+from mentos.maximum_entropy import get_params, maximum_entropy_pyomo_relaxed
+
+
+class TestMaximumEntropy(unittest.TestCase):
     def setUp(self):
         r = libsbml.SBMReader()
         sbml = r.readSBML("../models/sbml/split_pathway.xml")
-        n_ini, y_ini, beta_ini, target_log_vcounts, f_log_counts, S, K, obj_rxn_idx = get_params(sbml)
+        n_ini, y_ini, beta_ini, target_log_vcounts, f_log_counts, S, K, obj_rxn_idx = get_params(
+            sbml
+        )
 
     def test_maximum_entropy_pyomo_relaxed(self):
         maximum_entropy_pyomo_relaxed()
