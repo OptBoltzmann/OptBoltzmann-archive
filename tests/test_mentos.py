7996557c2a4eb07af3ebf7f99d27363e0dd99c1e
Jeremy Zucker <djinnome@gmail.com>
2022-06-28 15:49:44 -0700

    lint succeeded

--- tests/test_mentos.py
+++ tests/test_mentos.py
@@ -11,23 +11,33 @@ class TestMaximumEntropy(unittest.TestCase):
 
     def test_maximum_entropy_pyomo_relaxed(self):
         """Test against analytic solution"""
+
     def test_get_nullspace(self):
         """Make sure the nullspace is computed correctly"""
+
     def test_get_stoichiometric_matrix(self):
         """Make sure the stoichiometric matrix is extracted from the sbml file correctly"""
+
     def test_get_random_initial_variable_concentrations(self):
         """Make sure the initial variable concentrations pass muster"""
+
     def test_get_random_initial_fluxes(self):
         """Make sure the fluxes are initialized appropriately"""
+
     def test_get_standard_change_in_gibbs_free_energy(self):
         """Ensure the gibbs free energy is extracted from the sbml file correctly"""
+
     def test_get_initial_beta(self):
-        """Ensure the initial beta is consistent with the initial fluxes """
+        """Ensure the initial beta is consistent with the initial fluxes"""
+
     def test_get_target_log_variable_counts(self):
         """Ensure the log variable counts are extracted from the sbml file correctly"""
+
     def test_get_fixed_log_counts(self):
         """Ensure the fixed log counts are extracted from the sbml file correctly"""
+
     def test_get_objective_reaction_identifiers(self):
         """Ensure the flux objective coefficients are extracted from the sbml file correctly"""
+
     def test_get_params(self):
         """Ensure all parameters are accepted by maximum entropy pyomo"""
