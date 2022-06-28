0c3b24cea7c2f7417056b38a3675457d51c151bf
Jeremy Zucker <Jeremy.Zucker@pnnl.gov>
2022-06-28 15:38:22 -0700

    Added unit test stubs for all parameter initialization functions

--- tests/test_mentos.py
+++ tests/test_mentos.py
@@ -13,4 +13,23 @@ class TestMaximumEntropy(unittest.TestCase):
         maximum_entropy_pyomo_relaxed()
 
     def test_get_nullspace(self):
-        
+        """Make sure the nullspace is computed correctly"""
+    def test_get_stoichiometric_matrix(self):
+        """Make sure the stoichiometric matrix is extracted from the sbml file correctly"""
+    def test_get_random_initial_variable_concentrations(self):
+        """Make sure the initial variable concentrations pass muster"""
+    def test_get_random_initial_fluxes(self):
+        """Make sure the fluxes are initialized appropriately"""
+    def test_get_standard_change_in_gibbs_free_energy(self):
+        """Ensure the gibbs free energy is extracted from the sbml file correctly"""
+    def test_get_initial_beta(self):
+        """Ensure the initial beta is consistent with the initial fluxes """
+    def test_get_target_log_variable_counts(self):
+        """Ensure the log variable counts are extracted from the sbml file correctly"""
+    def test_get_fixed_log_counts(self):
+        """Ensure the fixed log counts are extracted from the sbml file correctly"""
+    def test_get_objective_reaction_identifiers(self):
+        """Ensure the flux objective coefficients are extracted from the sbml file correctly"""
+    def test_get_params(self):
+        """Ensure all parameters are accepted by maximum entropy pyomo"""
+    def test_
